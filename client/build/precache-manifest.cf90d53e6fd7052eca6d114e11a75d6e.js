cffe4680ee62d513dd02433babecd5ec96e99949
xujiang <xujiang156@qq.com>
2019-04-14 23:45:25 +0800

    xj

---
+++ client/build/precache-manifest.cf90d53e6fd7052eca6d114e11a75d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05ddb8013f796ecbdc7e",
+    "url": "./static/css/main.cdb04dd1.chunk.css"
+  },
+  {
+    "revision": "05ddb8013f796ecbdc7e",
+    "url": "./static/js/main.40d52fbf.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "139d0619e06663a5a372",
+    "url": "./static/css/2.da232d1d.chunk.css"
+  },
+  {
+    "revision": "139d0619e06663a5a372",
+    "url": "./static/js/2.55b2dec2.chunk.js"
+  },
+  {
+    "revision": "f5d5f902d3811c721fe170a4dc038bf0",
+    "url": "./index.html"
+  }
+];
